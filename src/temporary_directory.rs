571ac4a6769f18f9a37f88dde7c1eac2ea56a36a
tyler <tyler@cruftbusters.com>
2023-12-18 22:33:05 -0800

    test directory per unit test

--- src/temporary_directory.rs
+++ src/temporary_directory.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use uuid::Uuid;
 
 pub struct TemporaryDirectory {
     path: String,
@@ -6,13 +7,13 @@ pub struct TemporaryDirectory {
 
 impl TemporaryDirectory {
     pub fn new() -> TemporaryDirectory {
-        let test_directory = "/tmp/treesv-unit-tests";
-        fs::create_dir_all(test_directory).expect("to have created test directory");
-        TemporaryDirectory { path: String::from(test_directory) }
+        let path = format!("/tmp/treesv-unit-tests-{}", Uuid::new_v4());
+        fs::create_dir_all(&path).expect("to have created test directory");
+        TemporaryDirectory { path }
     }
 
     pub fn get_child_path(&mut self) -> String {
-        format!("{}/testfile", self.path)
+        format!("{}/{}", self.path, Uuid::new_v4())
     }
 }
 

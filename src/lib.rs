b9ae2f4b01d3fbc34350cc61fd08babd293c0f0b
tyler <tyler@cruftbusters.com>
2024-02-23 16:41:06 -0800

    blow away v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-mod balance;
-
-pub mod schema_sheet;
-pub mod serialization;
-
-#[cfg(test)]
-mod temporary_directory;
-
-#[cfg(test)]
-mod test_macro_rules;
 pub mod balance_sheet_v2;
 pub mod sheet_v2;
